6978cc2efe149b44db546ad33fbf74545264d3cf
JeonSoohyun <jeonsoohyun27@gmail.com>
2021-11-02 01:26:17 +0900

    검색창 찾기 & 검색어 입력 & 엔터

--- google.py
+++ google.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
-driver.get("http://www.python.org")
+chrome_path = r'/usr/local/bin/chromedriver'
+driver = webdriver.Chrome(executable_path=chrome_path)
+# driver = webdriver.Chrome('/usr/jeonsuhyeon/projects/selenium/chromedriver')
+driver.get("https://www.google.co.kr/imghp?hl=ko&ogbl")
+elem = driver.find_element_by_name("q") #검색창을 찾는 명령어
+elem.send_keys('치즈웨이브')
+elem.send_keys(Keys.RETURN)
+
 # assert "Python" in driver.title
 # elem = driver.find_element_by_name("q")
 # elem.clear()
@@ -10,3 +16,6 @@ driver.get("http://www.python.org")
 # elem.send_keys(Keys.RETURN)
 # assert "No results found." not in driver.page_source
 # driver.close()
+
+# chrome_path = r'/usr/local/bin/chromedriver' #path from 'which chromedriver'
+# driver = webdriver.Chrome(executable_path=chrome_path)
