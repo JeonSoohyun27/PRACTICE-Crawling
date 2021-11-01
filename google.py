e47abee53655e60081673c3b03fcce6009234b3f
JeonSoohyun <jeonsoohyun27@gmail.com>
2021-11-02 01:47:56 +0900

    반복문을 통해 계속적인 다운로드 과정을 만들어줌

--- google.py
+++ google.py
@@ -10,16 +10,19 @@ driver.get("https://www.google.co.kr/imghp?hl=ko&ogbl")
 elem = driver.find_element_by_name("q") #검색창을 찾는 명령어
 elem.send_keys('치즈웨이브') #검색어 입력
 elem.send_keys(Keys.RETURN) #검색창 엔터
-driver.find_elements_by_css_selector('.rg_i.Q4LuWd')[0].click() #[0]로 가장 첫번째 항목을 선택하고 .click으로 클릭해주기
+# driver.find_elements_by_css_selector('.rg_i.Q4LuWd')[0].click() #[0]로 가장 첫번째 항목을 선택하고 .click으로 클릭해주기
                                                                 #개발자도구로 사진에 대한 class 확인 및 붙여넣기 
                                                                 #여러개의 항목 선택시 find_element's' 를 사용
 
-time.sleep(2) #페이지를 로딩하는데 시간이 걸리므로 2초간 코드를 진행을 멈춰준다.
-imgUrl = driver.find_element_by_css_selector('.n3VNCb').get_attribute('src')  #이미지를 클릭했을때 나오는 세부이미지 class를 넣어주고 .get_attribute를 통해 src주소 다운로드 진행
+images = driver.find_elements_by_css_selector('.rg_i.Q4LuWd')
+count = 1
+for image in images:
+    image.click()
+    time.sleep(2) #페이지를 로딩하는데 시간이 걸리므로 2초간 코드를 진행을 멈춰준다.
+    imgUrl = driver.find_element_by_css_selector('.n3VNCb').get_attribute('src')  #이미지를 클릭했을때 나오는 세부이미지 class를 넣어주고 .get_attribute를 통해 src주소 다운로드 진행
                                                                             #print된 src 주소 https://s3-ap-northeast-1.amazonaws.com/dcreviewsresized/20210117170120_photo1_5f3a51b4b5f9.jpg
-
-urllib.request.urlretrieve(imgUrl,'test.jpg') #src주소를 다운받아서 test.jpg라는 이름으로 저장해주는 명령어
-
+    urllib.request.urlretrieve(imgUrl, str(count)+'.jpg') #src주소를 다운받아서 test.jpg라는 이름으로 저장해주는 명령어
+    count = count + 1
 
 # assert "Python" in driver.title
 # elem = driver.find_element_by_name("q")
